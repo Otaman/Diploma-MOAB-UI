ac02a5754521f38898ce37da90ceacb1ed346f3e
Otaman <otaman963@gmail.com>
2015-06-16 09:59:58 +0300

    Added /professional/jobs page data and controller method

---
+++ src/main/java/com/otaman/ProfessionalController.java
@@ -0,0 +1,33 @@
+package com.otaman;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProfessionalController {
+    @RequestMapping(value="/professional/jobs",
+            method = RequestMethod.GET)
+    public String showJobs(ModelMap model) {
+        return "professional/jobs";
+    }
+
+    @RequestMapping(value="/professional/create-job",
+            method = RequestMethod.GET)
+    public String createJob(ModelMap model) {
+        return "middle/create-job";
+    }
+
+    @RequestMapping(value="/professional/nodes",
+            method = RequestMethod.GET)
+    public String showNodes(ModelMap model) {
+        return "middle/nodes";
+    }
+
+    @RequestMapping(value="/professional/modify-node",
+            method = RequestMethod.GET)
+    public String modifyNode(ModelMap model) {
+        return "middle/modify-node";
+    }
+}
